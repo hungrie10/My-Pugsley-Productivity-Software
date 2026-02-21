be0f0e248136f4d0f072f616086dcd5a300fca37
Richard Mgbahurike <chocolatecookiepie387@gmail.com>
2026-02-21 07:57:15 +0100

    Auto commit: update project

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 function Home() {
   return (
-      <section>
-          
+    <section id='home_page_ outer' className='bg'>
+          <main id='home_page_inner'></main>
      </section>
   )
 }
