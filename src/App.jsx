8f2a18e51d3ea8e473e02202a0951589e0cab5cc
Richard Mgbahurike <chocolatecookiepie387@gmail.com>
2026-02-21 07:54:44 +0100

    Auto commit: update project

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
-import Home from './pages/Home'
+import Home from './pages/Home/Home'
 import Login from './pages/Login'
 import Workspace from './pages/Workspace'
 
