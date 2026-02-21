7c418f80671569951ac4141db9acbbd2acd359ab
Richard Mgbahurike <chocolatecookiepie387@gmail.com>
2026-02-21 08:17:19 +0100

    Auto commit: update project

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -5,8 +5,13 @@ function Home() {
   return (
     <section id='home_page_ outer'>
       <main id='home_page_inner'>
-        
-          </main>
+        <img src={mascot} alt="This is the image of the mascot for the productivity software" />
+          <h1>Pugsley</h1>
+          <p>Pugsley is a friendly productivity companion designed to help you organize your tasks, manage your time, and stay focused without the stress or overwhelm 
+          of traditional productivity tools.</p>
+        <br />
+        div
+      </main>
      </section>
   )
 }
