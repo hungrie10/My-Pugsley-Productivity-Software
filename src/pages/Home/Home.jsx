6deb6e3308ffc91e63934207e07dda8811e304b9
Richard Mgbahurike <chocolatecookiepie387@gmail.com>
2026-02-21 08:32:23 +0100

    Auto commit: update project

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,2 +1,14 @@
 import React from 'react'
-import mascot from './../../'
+import mascot from './../../assets/mascot.png'
+import './Home.css'
+
+
+function Home() {
+  return (
+    <section id="home_page_outer">
+        main      
+    </section>
+  )
+}
+
+export default Home
